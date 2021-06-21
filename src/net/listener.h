f112afd51ebc7285d678ebe31c36ee0245eddcdc
Marek Grzyb <grzybdev@gmail.com>
2021-06-21 14:24:04 +0200

    Started working on Listener abstract function

--- src/net/listener.h
+++ src/net/listener.h
@@ -3,11 +3,22 @@
 
 #include <QObject>
 
+enum LISTENER_STATUS {
+    LISTENER_SUCCESS,
+    LISTENER_FAILURE_PORT_USED,
+    LISTENER_FAILURE_UNKNOWN
+};
+
 class Listener : public QObject
 {
     Q_OBJECT
 public:
     explicit Listener(QObject *parent = nullptr);
+
+    bool isListening;
+
+    virtual LISTENER_STATUS StartListening() = 0;
+    virtual void StopListening() = 0;
 
 signals:
 
