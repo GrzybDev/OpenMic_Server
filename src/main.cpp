74bf0277434cf9843bc9644b4d4b73cc3d2c49aa
Marek Grzyb <grzybdev@gmail.com>
2021-06-29 14:28:41 +0200

    Reformatted main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,27 @@
 #include <QLocale>
 #include <QTranslator>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
 
-    QCoreApplication::setApplicationName("OpenMic Server");
-    QCoreApplication::setApplicationVersion(APP_VERSION);
-    QCoreApplication::setOrganizationDomain("grzybdev.github.io");
-    QCoreApplication::setOrganizationName("GrzybDev");
+	QCoreApplication::setApplicationName("OpenMic Server");
+	QCoreApplication::setApplicationVersion(APP_VERSION);
+	QCoreApplication::setOrganizationDomain("grzybdev.github.io");
+	QCoreApplication::setOrganizationName("GrzybDev");
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "OpenMic_Server_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
+	QTranslator translator;
+	const QStringList uiLanguages = QLocale::system().uiLanguages();
+	for (const QString& locale : uiLanguages)
+	{
+		const QString baseName = "OpenMic_Server_" + QLocale(locale).name();
+		if (translator.load(":/i18n/" + baseName))
+		{
+			QApplication::installTranslator(&translator);
+			break;
+		}
+	}
 
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+	MainWindow w;
+	return QApplication::exec();
 }
