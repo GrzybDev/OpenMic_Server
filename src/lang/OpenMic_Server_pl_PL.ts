e79507fc8ed61d762bc8e99b3993eb8b7f0b7716
Marek Grzyb <grzybdev@gmail.com>
2021-06-29 14:28:15 +0200

    Updated Polish translation

--- src/lang/OpenMic_Server_pl_PL.ts
+++ src/lang/OpenMic_Server_pl_PL.ts
@@ -2,11 +2,74 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="pl_PL">
 <context>
+    <name>Command</name>
+    <message>
+        <location filename="../net/listeners/command.cpp" line="14"/>
+        <source>OpenMic Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../net/listeners/command.cpp" line="15"/>
+        <source>Unable to start the server: %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../net/listeners/command.cpp" line="36"/>
+        <source>The server is running on
+
+IP: %1
+port: %2
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
+        <location filename="../ui/MainWindow/mainwindow.ui" line="14"/>
         <source>MainWindow</source>
-        <translation>Serwer OpenMic</translation>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="9"/>
+        <source>OpenMic First Run Setup</source>
+        <translation>Kreator pierwszej konfiguracji OpenMic</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="22"/>
+        <source>Welcome to OpenMic!</source>
+        <translation>Witaj w OpenMic!</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="24"/>
+        <source>It seems that this is first time you&apos;re launching OpenMic Server on your PC, and that&apos;s why you&apos;re seeing this setup wizard.
+
+This app is responsible for communicating with your mobile device via USB or local network (LAN) and playing received audio data to virtual microphone, so you can use your phone as microphone in your PC apps.
+
+Setup wizard will make sure that OpenMic can work correctly, please follow instructions for the best out-of-box experience.
+
+When you&apos;re ready, press &quot;Next&quot; to continue.</source>
+        <translation>Wygląda na to że uruchamiasz Serwer OpenMic po raz pierwszy na tym komputerze, z tego też powodu widzisz ten kreator.
+
+Ta aplikacja odpowiada za komunikację między twoim telefonem a komputerem za pomocą USB lub lokalnej sieci (LAN) oraz odtwarza odebrane dane audio do wirtualnego mikrofonu, co umożliwia użycie telefonu jako mikrofonu w aplikacjach na twoim komputerze.
+
+Kreator ten sprawdzi czy OpenMic będzie działał poprawnie na komputerze, proszę więc wykonywać instrukcje pokazane na ekranie aby twoje doświadczenie było jak najlepsze.
+
+Jeżeli jesteś gotowy, kliknij &quot;Dalej&quot; aby kontynuować.</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="39"/>
+        <source>Output Audio Device</source>
+        <translation>Urządzenie wyjściowe audio</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="41"/>
+        <source>This is the most essential part of setup, please make sure that output audio device is set to loopback audio device AND NOT to your actual headphones/speakers (if you&apos;re using Windows, we recommend using VB-Audio device, but any loopback device will work).</source>
+        <translation>To jest najważniejsza część procesu instalacji, proszę upewnić się że wybrane urządzenie audio jest (wirtualnym) urządzeniem pętli zwrotnej (loopback) A NIE twoimi słuchawkami/głośnikami (Jeżeli używasz Windowsa, zalecamy użycie sterownika VB-Audio, ale każde (wirtualne) urządzenie pętli zwrotnej zadziała).</translation>
     </message>
 </context>
 </TS>
