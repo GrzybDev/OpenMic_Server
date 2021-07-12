23bb0c78bdb0b7a18af8b8ed74cb2dcaaba4b99b
Marek Grzyb <grzybdev@gmail.com>
2021-07-12 17:33:57 +0200

    Updated Polish language file

--- src/lang/OpenMic_Server_pl_PL.ts
+++ src/lang/OpenMic_Server_pl_PL.ts
@@ -25,6 +25,87 @@ port: %2
     </message>
 </context>
 <context>
+    <name>Linux</name>
+    <message>
+        <location filename="../linux.cpp" line="15"/>
+        <source>Linux kernel module check</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="17"/>
+        <source>Failed to read %1 file!
+Please check that you have permission to read that file and try again!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="39"/>
+        <source>Linux kernel module loader</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="40"/>
+        <source>Cannot load kernel module!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="55"/>
+        <source>Failed to start authentication dialog!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="16"/>
+        <source>Failed to check if %1 kernel module is loaded!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="56"/>
+        <source>Failed to execute %1!
+This usually means that polkit is NOT installed on your system.
+
+You can still try to load this kernel module manually by executing command &apos;%2 %3&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="70"/>
+        <source>This application is not authorized to load kernel module.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="71"/>
+        <source>The calling process is not authorized or an authorization could not be obtained through authentication or an error occured.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="77"/>
+        <source>Authorization dialog was dismissed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="78"/>
+        <source>The authorization could not be obtained because the user dismissed the authentication dialog.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="84"/>
+        <source>
+
+(%1 exited with code: %2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="93"/>
+        <source>An unknown error occured!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../linux.cpp" line="94"/>
+        <source>
+
+Process exited with status code: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="../ui/MainWindow/mainwindow.ui" line="14"/>
@@ -35,17 +116,14 @@ port: %2
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../ui/SetupWizard/setupwizard.cpp" line="9"/>
         <source>OpenMic First Run Setup</source>
-        <translation>Kreator pierwszej konfiguracji OpenMic</translation>
+        <translation type="vanished">Kreator pierwszej konfiguracji OpenMic</translation>
     </message>
     <message>
-        <location filename="../ui/SetupWizard/setupwizard.cpp" line="22"/>
         <source>Welcome to OpenMic!</source>
-        <translation>Witaj w OpenMic!</translation>
+        <translation type="vanished">Witaj w OpenMic!</translation>
     </message>
     <message>
-        <location filename="../ui/SetupWizard/setupwizard.cpp" line="24"/>
         <source>It seems that this is first time you&apos;re launching OpenMic Server on your PC, and that&apos;s why you&apos;re seeing this setup wizard.
 
 This app is responsible for communicating with your mobile device via USB or local network (LAN) and playing received audio data to virtual microphone, so you can use your phone as microphone in your PC apps.
@@ -53,7 +131,7 @@ This app is responsible for communicating with your mobile device via USB or loc
 Setup wizard will make sure that OpenMic can work correctly, please follow instructions for the best out-of-box experience.
 
 When you&apos;re ready, press &quot;Next&quot; to continue.</source>
-        <translation>Wygląda na to że uruchamiasz Serwer OpenMic po raz pierwszy na tym komputerze, z tego też powodu widzisz ten kreator.
+        <translation type="vanished">Wygląda na to że uruchamiasz Serwer OpenMic po raz pierwszy na tym komputerze, z tego też powodu widzisz ten kreator.
 
 Ta aplikacja odpowiada za komunikację między twoim telefonem a komputerem za pomocą USB lub lokalnej sieci (LAN) oraz odtwarza odebrane dane audio do wirtualnego mikrofonu, co umożliwia użycie telefonu jako mikrofonu w aplikacjach na twoim komputerze.
 
@@ -62,14 +140,96 @@ Kreator ten sprawdzi czy OpenMic będzie działał poprawnie na komputerze, pros
 Jeżeli jesteś gotowy, kliknij &quot;Dalej&quot; aby kontynuować.</translation>
     </message>
     <message>
-        <location filename="../ui/SetupWizard/setupwizard.cpp" line="39"/>
         <source>Output Audio Device</source>
-        <translation>Urządzenie wyjściowe audio</translation>
+        <translation type="vanished">Urządzenie wyjściowe audio</translation>
     </message>
     <message>
-        <location filename="../ui/SetupWizard/setupwizard.cpp" line="41"/>
         <source>This is the most essential part of setup, please make sure that output audio device is set to loopback audio device AND NOT to your actual headphones/speakers (if you&apos;re using Windows, we recommend using VB-Audio device, but any loopback device will work).</source>
-        <translation>To jest najważniejsza część procesu instalacji, proszę upewnić się że wybrane urządzenie audio jest (wirtualnym) urządzeniem pętli zwrotnej (loopback) A NIE twoimi słuchawkami/głośnikami (Jeżeli używasz Windowsa, zalecamy użycie sterownika VB-Audio, ale każde (wirtualne) urządzenie pętli zwrotnej zadziała).</translation>
+        <translation type="vanished">To jest najważniejsza część procesu instalacji, proszę upewnić się że wybrane urządzenie audio jest (wirtualnym) urządzeniem pętli zwrotnej (loopback) A NIE twoimi słuchawkami/głośnikami (Jeżeli używasz Windowsa, zalecamy użycie sterownika VB-Audio, ale każde (wirtualne) urządzenie pętli zwrotnej zadziała).</translation>
+    </message>
+</context>
+<context>
+    <name>SetupWizard</name>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="8"/>
+        <source>OpenMic First Run Setup</source>
+        <translation type="unfinished">Kreator pierwszej konfiguracji OpenMic</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="30"/>
+        <source>Welcome to OpenMic!</source>
+        <translation type="unfinished">Witaj w OpenMic!</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="32"/>
+        <source>It seems that this is first time you&apos;re launching OpenMic Server on your PC, and that&apos;s why you&apos;re seeing this setup wizard.
+
+This app is responsible for communicating with your mobile device via USB or local network (LAN) and playing received audio data to virtual microphone, so you can use your phone as microphone in your PC apps.
+
+Setup wizard will make sure that OpenMic can work correctly, please follow instructions for the best out-of-box experience.
+
+When you&apos;re ready, press &quot;Next&quot; to continue.</source>
+        <translation type="unfinished">Wygląda na to że uruchamiasz Serwer OpenMic po raz pierwszy na tym komputerze, z tego też powodu widzisz ten kreator.
+
+Ta aplikacja odpowiada za komunikację między twoim telefonem a komputerem za pomocą USB lub lokalnej sieci (LAN) oraz odtwarza odebrane dane audio do wirtualnego mikrofonu, co umożliwia użycie telefonu jako mikrofonu w aplikacjach na twoim komputerze.
+
+Kreator ten sprawdzi czy OpenMic będzie działał poprawnie na komputerze, proszę więc wykonywać instrukcje pokazane na ekranie aby twoje doświadczenie było jak najlepsze.
+
+Jeżeli jesteś gotowy, kliknij &quot;Dalej&quot; aby kontynuować.</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="48"/>
+        <source>Linux Kernel Module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="50"/>
+        <source>You&apos;re running this app under Linux based operating system. OpenMic app can try to load kernel module `snd-aloop` on launch. This module adds special virtual loopback audio devices that OpenMic will use to send audio data.
+
+Note: OpenMic on Linux is designed to work with `snd-aloop` module, but it&apos;s not required, any loopback device will work (or even your actual headphones/speakers, depends on your need).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="56"/>
+        <source>Load `snd-aloop` on application start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="58"/>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="82"/>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="106"/>
+        <source>When you&apos;re ready, click &quot;Next&quot; to continue.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="73"/>
+        <source>Output Audio Device</source>
+        <translation type="unfinished">Urządzenie wyjściowe audio</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="75"/>
+        <source>This is the most essential part of setup, please make sure that output audio device is set to loopback audio device AND NOT to your actual headphones/speakers (if you&apos;re using Windows, we recommend using VB-Audio device, but any loopback device will work).</source>
+        <translation type="unfinished">To jest najważniejsza część procesu instalacji, proszę upewnić się że wybrane urządzenie audio jest (wirtualnym) urządzeniem pętli zwrotnej (loopback) A NIE twoimi słuchawkami/głośnikami (Jeżeli używasz Windowsa, zalecamy użycie sterownika VB-Audio, ale każde (wirtualne) urządzenie pętli zwrotnej zadziała).</translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="97"/>
+        <source>Network Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="99"/>
+        <source>Now, select your network card, it should be connected to the same network as your phone. (Note: If you&apos;ll be using USB communication you can skip this step by clicking Next)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="121"/>
+        <source>You&apos;re all set!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/SetupWizard/setupwizard.cpp" line="123"/>
+        <source>That&apos;s it! OpenMic Server is now ready for the action!&lt;br/&gt;&lt;br/&gt;If you haven&apos;t already, download &quot;OpenMic Client&quot; from Google Play! (&lt;a href=&quot;https://grzybmic.web.app/googleplay&quot;&gt;https://grzybmic.web.app/googleplay&lt;/a&gt;)&lt;br/&gt;&lt;br/&gt;To start using this app select &quot;Connect&quot; from Device menu and follow on-screen instructions.&lt;br/&gt;&lt;br/&gt;I hope you&apos;ll find this app useful!</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
